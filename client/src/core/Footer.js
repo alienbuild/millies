296da44438b34f84cd122b8ef4b8a09bfde7c50e
Liam K Franklin <lk@alienbuild.uk>
2020-02-14 17:08:37 +0000

    Remove retired classnames

--- client/src/core/Footer.js
+++ client/src/core/Footer.js
@@ -2,15 +2,9 @@ import React, {Fragment} from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => (
-    <Fragment>
-        <footer id="footer-primary">
-            <div className="container">
-                <div className="footer-grid">
-                    Footer
-                </div>
-            </div>
-        </footer>
-    </Fragment>
+    <footer>
+        Footer
+    </footer>
 );
 
 export default Footer;
--- client/src/core/Header.js
+++ client/src/core/Header.js
@@ -4,30 +4,27 @@ import { cartTotal } from "./cartUtils";
 import { signout, isAuthenticated } from "../auth";
 
 const Header = ({ history }) => (
-    <header id="header-primary">
-            <div className="header__grid">
-                Header
-                <nav className="header__nav">
-                    <ul>
-                        {/*(TODO: Add Authentication check to show/hide links)*/}
-                        {/*User is not authenticated*/}
-                        {!isAuthenticated() && (
-                            <Fragment>
-                                <li className="header__item"><Link to="/signin" className="header__item-link">Login</Link></li>
-                            </Fragment>
-                        )}
-                        {/*User is authenticated*/}
-                        {isAuthenticated() && (
-                            <Fragment>
-                                <li className="header__item"><button className="header__item-link" onClick={(e) => signout(() => {
-                                    history.push('/');
-                                })}>Log out</button></li>
-                            </Fragment>
-                        )}
-                    </ul>
-                </nav>
-                <span>Cart <sup>{cartTotal()}</sup> </span>
-            </div>
+    <header>
+        <nav>
+            <ul>
+                {/*(TODO: Add Authentication check to show/hide links)*/}
+                {/*User is not authenticated*/}
+                {!isAuthenticated() && (
+                    <Fragment>
+                        <li><Link to="/signin">Login</Link></li>
+                    </Fragment>
+                )}
+                {/*User is authenticated*/}
+                {isAuthenticated() && (
+                    <Fragment>
+                        <li><button onClick={(e) => signout(() => {
+                            history.push('/');
+                        })}>Log out</button></li>
+                    </Fragment>
+                )}
+            </ul>
+        </nav>
+        <span>Cart <sup>{cartTotal()}</sup> </span>
     </header>
 );
 
