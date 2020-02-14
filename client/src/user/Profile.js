3067ecd3b8760d406dfcd4f80d1d8acc3c7c0249
Liam K Franklin <lk@alienbuild.uk>
2020-02-14 20:28:38 +0000

    Get user info for profile update

--- client/src/user/Profile.js
+++ client/src/user/Profile.js
@@ -1,12 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Link } from "react-router-dom";
 import Default from '../layouts/Default';
 import { isAuthenticated } from "../auth";
+import { read, update, updateUser } from "./apiUser";
+import Search from "../core/Search";
+import Card from "../core/Card";
 
-const Profile = () => {
+const Profile = ({match}) => {
+    // Init state
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        // TODO: Add more as necessary
+        error: false,
+        success: false
+    });
+
+    const {token} = isAuthenticated();
+
+    const { name, email, password, error, success } = values;
+
+    const init = (userId) => {
+        console.log('User ID is:', userId);
+        read(userId, token)
+            .then(data => {
+                if (data.error){
+                    setValues({...values, error: true})
+                } else {
+                    setValues({...values, name: data.name, email: data.email})
+                }
+            })
+    };
+
+    useEffect(() => {
+        init(match.params.userId);
+    }, []);
+
     return(
-        <h2>Profile</h2>
+        <Default title="Profile" description="Profile description">
+            <h2>Profile update.</h2>
+            {JSON.stringify(values)}
+        </Default>
     )
+
 };
 
 export default Profile;
